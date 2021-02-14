c66a3ddfcb8dca52b111f941fe872130fe6111bd
carloslevo <madrilevo16@gmail.com>
2021-02-14 12:26:18 +0100

    1st commit from local rep

---
+++ src/test/java/com/project/tests/Carlos.java
@@ -0,0 +1,11 @@
+package com.project.tests;
+
+import org.testng.annotations.Test;
+
+public class Carlos {
+
+    @Test
+    public void test1(){
+        System.out.println("Hola a todos");
+    }
+}
